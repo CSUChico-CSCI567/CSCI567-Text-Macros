4b70da02865382b4fca6388fb80c6b7e23348e23
NanjoW <nanjow@gmail.com>
2016-05-15 12:42:25 -0700

    Add object

---
+++ TextMacro/app/src/main/java/com/example/sterling/textmacro/Objects/TextMacros.java
@@ -0,0 +1,28 @@
+package com.example.sterling.textmacro.Objects;
+
+import com.orm.SugarRecord;
+
+/**
+ * Created by Nanaka on 5/15/16.
+ */
+public class TextMacros extends SugarRecord{
+    //String PhoneNumber; //For number specific if want to implement
+    String up;
+    String down;
+
+    public String getUp() {
+        return up;
+    }
+
+    public void setUp(String up) {
+        this.up = up;
+    }
+
+    public String getDown() {
+        return down;
+    }
+
+    public void setDown(String down) {
+        this.down = down;
+    }
+}

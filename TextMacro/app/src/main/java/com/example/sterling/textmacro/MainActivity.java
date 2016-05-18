2e011e93984802318680d14845de3a74240fc190
NanjoW <nanjow@gmail.com>
2016-05-17 22:49:55 -0700

    fix the crashing and make home screen settings

--- TextMacro/app/src/main/java/com/example/sterling/textmacro/MainActivity.java
+++ TextMacro/app/src/main/java/com/example/sterling/textmacro/MainActivity.java
@@ -13,15 +13,49 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.NotificationCompat;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.sterling.textmacro.Objects.TextMacros;
+import com.orm.SugarContext;
+
+import org.w3c.dom.Text;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends Activity {
 
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_SMS}, 123);
+    @Bind(R.id.negTxt)
+    EditText negTxt;
+
+    @Bind(R.id.posTxt)
+    EditText posTxt;
+
+    @OnClick(R.id.insertBtn)
+    public void insert(View v){
+        TextMacros txt = TextMacros.findById(TextMacros.class, 1);
+        if(txt == null){
+            txt = new TextMacros();
+        }
+        txt.setUp(posTxt.getText().toString());
+        txt.setDown(negTxt.getText().toString());
+        txt.save();
         Intent intent = new Intent(this, SmsActivity.class);
         startActivity(intent);
     }
 
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SugarContext.init(this);
+        ButterKnife.bind(this);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_SMS}, 123);
+    }
+
+    @Override
+    protected void onDestroy() {
+        SugarContext.terminate();
+        super.onDestroy();
+    }
 }

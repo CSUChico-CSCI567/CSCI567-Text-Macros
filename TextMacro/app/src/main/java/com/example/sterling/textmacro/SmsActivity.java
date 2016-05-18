bd934e3df77e0dde568bf05416f436181f6e241b
Sterling Baldwin <sterling16@mac.com>
2016-05-18 15:52:31 -0700

    App launches default on notification click

--- TextMacro/app/src/main/java/com/example/sterling/textmacro/SmsActivity.java
+++ TextMacro/app/src/main/java/com/example/sterling/textmacro/SmsActivity.java
@@ -21,12 +21,9 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class SmsActivity extends Activity implements OnItemClickListener {
+public class SmsActivity extends Activity{
 
     private static SmsActivity inst;
-    ArrayList<String> smsMessagesList = new ArrayList<String>();
-    ListView smsListView;
-    ArrayAdapter arrayAdapter;
 
     public static SmsActivity instance() {
         return inst;
@@ -42,47 +39,7 @@ public class SmsActivity extends Activity implements OnItemClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sms);
-        smsListView = (ListView) findViewById(R.id.SMSList);
-        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, smsMessagesList);
-        smsListView.setAdapter(arrayAdapter);
-        smsListView.setOnItemClickListener(this);
 
-        refreshSmsInbox();
     }
 
-    public void refreshSmsInbox() {
-        ContentResolver contentResolver = getContentResolver();
-        Cursor smsInboxCursor = contentResolver.query(Uri.parse("content://sms/inbox"), null, null, null, null);
-        int indexBody = smsInboxCursor.getColumnIndex("body");
-        int indexAddress = smsInboxCursor.getColumnIndex("address");
-        if (indexBody < 0 || !smsInboxCursor.moveToFirst()) return;
-        arrayAdapter.clear();
-        do {
-            String str = "SMS From: " + smsInboxCursor.getString(indexAddress) +
-                    "\n" + smsInboxCursor.getString(indexBody) + "\n";
-            arrayAdapter.add(str);
-        } while (smsInboxCursor.moveToNext());
-    }
-
-    public void updateList(final String smsMessage) {
-        arrayAdapter.insert(smsMessage, 0);
-        arrayAdapter.notifyDataSetChanged();
-    }
-
-    public void onItemClick(AdapterView<?> parent, View view, int pos, long id) {
-        try {
-            String[] smsMessages = smsMessagesList.get(pos).split("\n");
-            String address = smsMessages[0];
-            String smsMessage = "";
-            for (int i = 1; i < smsMessages.length; ++i) {
-                smsMessage += smsMessages[i];
-            }
-
-            String smsMessageStr = address + "\n";
-            smsMessageStr += smsMessage;
-            Toast.makeText(this, smsMessageStr, Toast.LENGTH_SHORT).show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 }
